3cd7976151491a88e3657f8e7d800f01c584565b
ishaqadhel <ishaq.adhel@gmail.com>
2021-05-08 21:13:45 +0700

    feat(soal1): add register feature

--- soal1/Client/client.c
+++ soal1/Client/client.c
@@ -6,36 +6,69 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #define PORT 8080
-  
-int main(int argc, char const *argv[]) {
-    struct sockaddr_in address;
-    int sock = 0, valread;
+
+struct sockaddr_in address;
+int sock = 0, valread;
+
+int main()
+{
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
     char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         printf("\n Socket creation error \n");
         return -1;
     }
-  
+
     memset(&serv_addr, '0', sizeof(serv_addr));
-  
+
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-      
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
-  
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
         printf("\nConnection Failed \n");
         return -1;
     }
 
-    send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
-    valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );
+    char menu[255], id[255], password[255];
+    memset(menu, 0, sizeof(menu)); 
+    char idPassword[1024] = {0};
+
+    printf("--Main Menu--\n");
+    printf("1. Register\n");
+    printf("2. Login\n");
+    printf("Your Choice (1/2) : ");
+    scanf("%s", menu);
+    send(sock, menu, strlen(menu), 0);
+    valread = read(sock, buffer, 1024);
+
+    // printf("%s\n", buffer);
+
+    if (strcmp(buffer, "register") == 0)
+    {
+        printf("\n--Register--\n");
+        printf("id: ");
+        scanf("%s", id);
+        printf("\npassword: ");
+        scanf("%s", password);
+        sprintf(idPassword, "%s:%s\n", id, password);
+        send(sock, idPassword, strlen(idPassword), 0);
+        printf("\nregister success\n");
+    }
+
+    else if (strcmp(buffer, "login") == 0) {
+        printf("\n--Login--\n");
+    }
+
+    else {
+        printf("Wrong Choice !\n");
+    }
     return 0;
 }
--- soal1/Server/server.c
+++ soal1/Server/server.c
@@ -4,48 +4,97 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #define PORT 8080
 
-int main(int argc, char const *argv[]) {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+int server_fd, new_socket, valread;
+struct sockaddr_in address;
+int addrlen = sizeof(address);
+FILE *fp;
+
+typedef struct account
+{
+    char id[255];
+    char password[255];
+} account;
+
+void *app(void *arg)
+{
+    int new_socket;
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
     char buffer[1024] = {0};
-    char *hello = "Hello from server";
-      
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    while (1)
+    {
+        char idPassword[255] = {0};
+        memset(buffer, 0, sizeof(buffer)); 
+        valread = read(new_socket, buffer, 1024);
+
+        if(strcmp(buffer, "register") == 0) {
+            fp = fopen("akun.txt", "a");
+            send(new_socket, buffer, strlen(buffer), 0);
+            memset(buffer, 0, sizeof(buffer)); 
+            
+            read(new_socket, buffer, 1024);
+            fputs(buffer, fp);
+            fclose(fp);
+            printf("register success\n");
+        }
+
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int opt = 1;
+    char buffer[1024] = {0};
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-      
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-      
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0)
+    {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+    pthread_t threads[100];
+    for (int i = 0; i < 100; i++)
+    {
+        int *new_val = &i;
+        pthread_create(&threads[i], NULL, app, (void *)new_val); //menjalankan fungsi untuk menghandle pemain
+    }
+
+    for (int i = 0; i < 100; i++)  
+        pthread_join(threads[i], NULL); 
+
     return 0;
 }
