316f84e9082498de4f664ab632ae4f69bcb9a1e0
Salman Damai Alfariq <73969921+SalmanDA@users.noreply.github.com>
2021-05-23 15:09:37 +0700

    Perbaiki soal2a

--- soal2/soal2a.c
+++ soal2/soal2a.c
@@ -2,14 +2,24 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <stdlib.h>
 int main()
 {
 	key_t key = 1234;
-	int *value;
-	int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-	value = shmat(shmid, NULL, 0);
+	int (*value)[6];
+	int shmid = shmget(key, sizeof(int[4][6]), IPC_CREAT | 0666);
+  	value = shmat(shmid, NULL, 0);
 
-	int a[4][3],b[3][6],c[4][6]={0};
+//	int a[4][3] = {{1,2,3},
+//			{1,2,3},
+//			{1,2,3},
+//			{0,0,0}};
+//	int b[3][6] = {{1,2,3,1,2,3},
+//			{1,2,3,1,2,3},
+//			{0,0,0,0,0,0}};
+
+	int a[4][3], b[3][6];
+	int c[4][6]={0};
 	int i,j,k;
 
 	for(i=0;i<4;i++){
@@ -27,24 +37,18 @@ int main()
 	for(i=0;i<4;i++){
 		for(j=0;j<6;j++){
 			for(k=0;k<3;k++){
-				c[i][j] += a[i][k]*b[k][j];
+				value[i][j] += a[i][k]*b[k][j];
 			}
 		}
 	}
 
 	for(i=0;i<4;i++){
 		for(j=0;j<6;j++){
-			printf("%d ",c[i][j]);
+			printf("%d\t",value[i][j]);
 		}
 		printf("\n");
 	}
 
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 5; j++){
-			*value = c[i][j];
-			sleep(1);
-		}
-	}
-	shmdt(value);
-	shmctl(shmid, IPC_RMID, NULL);
+//	shmdt(value);
+//	shmctl(shmid, IPC_RMID, NULL);
 }
